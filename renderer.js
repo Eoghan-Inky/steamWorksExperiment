1706805bd988bb97f37c4c0d7cc550f8a8db85cb
Owen <eoghan@inky-pen.com>
2020-08-15 17:09:03 +0200

    Add EdgeJS support (Run .NET code)
    Had to downgrade to Electron 8.2.3 for this.

--- renderer.js
+++ renderer.js
@@ -4,3 +4,17 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+var edge = require('electron-edge-js');
+
+var helloWorld = edge.func(function () {/*
+    async (input) => {
+        return ".NET Welcomes " + input.ToString();
+    }
+*/});
+
+helloWorld('JavaScript', function (error, result) {
+    if (error) throw error;
+    var expDiv = document.getElementById("experiment");
+    expDiv.innerText = result;
+});
