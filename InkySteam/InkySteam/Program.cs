b40843b9d0954ff04f0087c14f885dc674f32e96
Owen <eoghan@inky-pen.com>
2020-08-15 16:04:48 +0200

    SteamWorks .NET experiment

---
+++ InkySteam/InkySteam/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using Steamworks;
+
+
+namespace InkySteam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Starts the app...
+            SteamAPI.RestartAppIfNecessary((Steamworks.AppId_t)681220);
+
+            bool initialized = Steamworks.SteamAPI.Init();
+
+            Console.WriteLine("Initialized: " + initialized);
+
+            //Console.WriteLine("Steam User ID: " + SteamUser.GetSteamID());
+
+            SteamManager sm = new SteamManager();
+
+            SteamAPI.Shutdown();
+        }
+    }
+}
---
+++ InkySteam/InkySteam/SteamManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Steamworks;
+
+namespace InkySteam
+{
+    class SteamManager
+    {
+
+        public SteamManager()
+        {
+            Console.WriteLine("constructor");
+            if (!Packsize.Test())
+            {
+                throw new Exception("[Steamworks.NET] Packsize Test returned false, the wrong version of Steamworks.NET is being run in this platform.");
+            }
+
+            if (!DllCheck.Test())
+            {
+                throw new Exception("[Steamworks.NET] DllCheck Test returned false, One or more of the Steamworks binaries seems to be the wrong version.");
+            }
+        }
+
+
+        private void Awake()
+        {
+            try
+            {
+                if (SteamAPI.RestartAppIfNecessary((Steamworks.AppId_t)681220))
+                {
+                    //Application.Quit();
+                    return;
+                }
+            }
+            catch (System.DllNotFoundException e)
+            {
+                //Application.Quit();
+                throw new Exception("[Steamworks.NET] Could not load [lib]steam_api.dll/so/dylib. It's likely not in the correct location. Refer to the README for more details.", e);
+            }
+        }
+    }
+}
